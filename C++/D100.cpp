a009c6d8c970d9342a7d1a3af01265dd86573df9
HangDDev <155066834+HangDDev@users.noreply.github.com>
2024-10-13 02:07:42 +0800

    Create D100.cpp

---
+++ C++/D100.cpp
@@ -0,0 +1,10 @@
+// https://judge.hkoi.org/task/D100
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int num1, num2;
+  cin >> num1 >> num2;
+  cout << num1 + num2;
+  return 0;
+}
